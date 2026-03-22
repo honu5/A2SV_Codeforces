37c1401f54ab492a5b06998a3725029e5cd25ac9
honu5 <liranso392@gmail.com>
2026-03-22 20:45:04 +0300

    Solution for 1709 problem

--- B_Polycarp_Training.py
+++ B_Polycarp_Training.py
@@ -2,14 +2,8 @@ n=int(input())
 lst=list(map(int,input().split()))
 lst.sort()
 val=0
-if len(lst)==1 and lst[0]>=1:
-    print(1)
-else:
-    i=0
-    while i<len(lst):
-        if lst[i]>=val:
-            val+=1
-            i+=1
-        else:   
-            i+=1
+for i in lst:
+    if i>val:
+        val+=1
 print(val)
+    
